a33f4bd25df512a4f213e83b3dce3c45126146fa
eldarial <darialdo92@hotmail.com>
2017-02-27 20:49:25 -0500

    final updates

--- advance_lane.py
+++ advance_lane.py
@@ -307,7 +307,7 @@ def process_frame(input_image):
     # Calculate the new radii of curvature
     left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
     right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
-    cv2.putText(output_image,'curvature: {0} m'.format(int(left_curverad)),(10,70), font, 2,(255,255,255),2)
+    cv2.putText(output_image,'curvature left: {0} m'.format(int(left_curverad)),(10,70), font, 2,(255,255,255),2)
 
     #how far is the car from the middle of the road
     x1_meters = left_fitx[-1]*xm_per_pix
